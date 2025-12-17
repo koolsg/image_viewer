7307e1c2384572a747691acff70534e9c4633c4e
BlackBigChicken <blackbigchicken@gmail.com>
2025-12-17 20:13:39 +0900

    cleanup: remove migrations compatibility shim; update script to import from db.migrations

--- image_viewer/image_engine/migrations.py
+++
@@ -1,6 +0,0 @@
-"""Compatibility shim re-exporting migrations implementation from
-`image_viewer.image_engine.db.migrations`."""
-
-from image_viewer.image_engine.db.migrations import apply_migrations, get_latest_version
-
-__all__ = ["apply_migrations", "get_latest_version"]
